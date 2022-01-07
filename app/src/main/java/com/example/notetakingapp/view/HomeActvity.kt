f5ed8633baa57d0944f7e95c9429c8fa2fc62103
niranjannlc <niranjannlc10@gmail.com>
2022-01-07 16:50:17 +1200

    creating architecture of mvvm

--- app/src/main/java/com/example/notetakingapp/view/HomeActvity.kt
+++ app/src/main/java/com/example/notetakingapp/view/HomeActvity.kt
@@ -1,9 +1,10 @@
-package com.example.notetakingapp
+package com.example.notetakingapp.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.notetakingapp.R
 
-class MainActivity : AppCompatActivity() {
+class HomeActvity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
