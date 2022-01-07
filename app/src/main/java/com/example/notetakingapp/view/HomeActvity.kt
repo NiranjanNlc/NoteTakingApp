8513d2aed440801ad4f4daed8fa8f7b9d5aaccfe
niranjannlc <niranjannlc10@gmail.com>
2022-01-07 16:58:49 +1200

    enabled vew and data binding in lsyout snd actvity

--- app/src/main/java/com/example/notetakingapp/view/HomeActvity.kt
+++ app/src/main/java/com/example/notetakingapp/view/HomeActvity.kt
@@ -2,11 +2,16 @@ package com.example.notetakingapp.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.example.notetakingapp.R
+import com.example.notetakingapp.databinding.ActivityMainBinding
 
-class HomeActvity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class HomeActvity : AppCompatActivity()
+{
+    private lateinit var binding : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding =   DataBindingUtil.setContentView(this,R.layout.activity_main)
     }
 }
