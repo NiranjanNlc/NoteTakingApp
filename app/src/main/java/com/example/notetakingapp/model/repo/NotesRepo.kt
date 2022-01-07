9adfd2dc19f15db59e2d887f5b74f1b0760e7851
niranjannlc <niranjannlc10@gmail.com>
2022-01-07 18:26:07 +1200

    notes repo completed

---
+++ app/src/main/java/com/example/notetakingapp/model/repo/NotesRepo.kt
@@ -0,0 +1,33 @@
+package com.example.notetakingapp.model.repo
+
+import androidx.lifecycle.LiveData
+import com.example.notetakingapp.model.dao.NotesDao
+import com.example.notetakingapp.model.data.Notes
+
+class NotesRepo(private val notesDao: NotesDao) {
+
+    val getAllData = notesDao.getAllData()
+    val sortByHighPriority = notesDao.sortByHighPriority()
+    val sortByLowPriority = notesDao.sortByLowPriority()
+
+    suspend fun insertData(notes: Notes) {
+        notesDao.insertData(notes)
+    }
+
+    suspend fun updateData(notes: Notes) {
+        notesDao.updateData(notes)
+    }
+
+    suspend fun deleteData(notes: Notes) {
+        notesDao.deleteData(notes)
+    }
+
+    suspend fun deleteAllData() {
+        notesDao.deleteAllData()
+    }
+
+    fun searchData(query: String): LiveData<List<Notes>> {
+        return notesDao.searchData(query)
+    }
+}
+    
