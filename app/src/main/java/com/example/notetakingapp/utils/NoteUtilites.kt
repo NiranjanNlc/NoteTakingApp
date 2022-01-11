eae7f4ebb5caa2fff3595d44986f749fa89acd22
niranjannlc <niranjannlc10@gmail.com>
2022-01-11 18:55:29 +1200

    refactoring .............

--- app/src/main/java/com/example/notetakingapp/utils/NoteUtilites.kt
+++ app/src/main/java/com/example/notetakingapp/utils/NoteUtilites.kt
@@ -4,25 +4,25 @@ import com.example.notetakingapp.model.data.Fidility
 import com.example.notetakingapp.model.data.Notes
 
 object NoteUtilites
-{
-
-    fun initiateDtaBase()
-    {
-        var descriptionList = listOf<String>(" Niranjan lamichhane ",
-        " ashmita thapa       ",
-        " sashmita neupane    ",
-        " rachana poudel ",
-        " jharana bajracharya  ",
-        " Ashma Baniya ",
-        " ")
-        descriptionList.forEach {
-            insertData(it)
-        }
-    }
-
-    private fun insertData(sesc: String)
-    {
-
-
-    }
-}
+{}
+//
+//    fun initiateDtaBase()
+//    {
+//        var descriptionList = listOf<String>(" Niranjan lamichhane ",
+//        " ashmita thapa       ",
+//        " sashmita neupane    ",
+//        " rachana poudel ",
+//        " jharana bajracharya  ",
+//        " Ashma Baniya ",
+//        " ")
+//        descriptionList.forEach {
+//            insertData(it)
+//        }
+//    }
+//
+//    private fun insertData(sesc: String)
+//    {
+//
+//
+//    }
+//}
