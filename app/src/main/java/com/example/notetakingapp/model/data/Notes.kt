58e2d388625a3e1823a59439c977fd0e13ea16b3
niranjannlc <niranjannlc10@gmail.com>
2022-01-29 20:10:38 +0545

    simplfying ldatabinding

--- app/src/main/java/com/example/notetakingapp/model/data/Notes.kt
+++ app/src/main/java/com/example/notetakingapp/model/data/Notes.kt
@@ -8,7 +8,7 @@ import java.io.Serializable
 @Entity(tableName = "notes_table")
 data class Notes(
     @PrimaryKey(autoGenerate = true)
-    var id: Int,
+    var id: Int?=null,
     var title: String?=null,
     var fidility: Fidility?=null,
     var description: String?=null,
